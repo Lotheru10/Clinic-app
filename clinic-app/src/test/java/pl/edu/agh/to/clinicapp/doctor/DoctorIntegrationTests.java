e35969e7f7ec6cde7185e878a50d9e3693b24c2f
Lotheru10 <jakubprygiel72@gmail.com>
2025-12-15 16:58:34 +0100

    minor fix

--- clinic-app/src/test/java/pl/edu/agh/to/clinicapp/doctor/DoctorIntegrationTests.java
+++ clinic-app/src/test/java/pl/edu/agh/to/clinicapp/doctor/DoctorIntegrationTests.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-gitimport org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
