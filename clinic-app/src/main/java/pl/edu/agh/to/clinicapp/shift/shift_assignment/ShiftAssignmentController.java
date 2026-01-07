dc05634fefb945e696c8aab4a868fa0cc4482d45
Harry <harry@Harrys-Air.mshome.net>
2026-01-07 22:28:59 +0100

    add shift assignment controller java docs

--- clinic-app/src/main/java/pl/edu/agh/to/clinicapp/shift/shift_assignment/ShiftAssignmentController.java
+++ clinic-app/src/main/java/pl/edu/agh/to/clinicapp/shift/shift_assignment/ShiftAssignmentController.java
@@ -1,5 +1,9 @@
 package pl.edu.agh.to.clinicapp.shift.shift_assignment;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +15,7 @@ import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/shift-assignment")
+@Tag(name = "Shift assignment", description = "API for displaying free doctors and doctor's offices")
 public class ShiftAssignmentController {
     private final ShiftAssignmentService shiftAssignmentService;
 
@@ -19,6 +24,13 @@ public class ShiftAssignmentController {
     }
 
 
+    @Operation(
+            summary = "Returns available doctors and offices",
+            description = "Returns list of free doctors and free doctor's offices in a given period of time")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "List retrieved successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+    })
 
     @GetMapping("/avaibility")
     public AvailabilityDTO getAvailability(
